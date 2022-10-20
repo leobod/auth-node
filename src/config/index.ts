1e3f2cd70a59205aa4ee07b7bebf2cb1ab4c6f37
leobod <leobod@dingtalk.com>
2022-10-20 11:36:30 +0800

    chore: 引入mysql

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+
+import {mysql_config} from "./database";
+// const mysql_config = {
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '123456',
+//   database : 'demo'
+// }
+
+export {
+  mysql_config
+}
