ff80939b2f40118445912aa15cc0d20927358281
leobod <leobod@dingtalk.com>
2022-10-21 18:25:14 +0800

    feat: `results as Array<any>` 类型断言案例

--- test/mysqlQuery.test.ts
+++ test/mysqlQuery.test.ts
@@ -1,12 +1,23 @@
 
 
 import {getConnectionFromPool} from "../src/common/RDB";
+import {toCamelCase} from "../src/utils";
 
 let connection = null
 let tableList = []
 getConnectionFromPool('SELECT SQL_CALC_FOUND_ROWS * FROM customer_login')
   .then(results => {
     console.log(results);
+    const dataList = (results as Array<any>).map(item => {
+      const keys = Object.keys(item);
+      const camelCaseKeys = keys.map(toCamelCase);
+      const model = {}
+      for (let i = 0; i < keys.length; ++i) {
+        model[camelCaseKeys[i]] = item[keys[i]];
+      }
+      return model;
+    });
+    console.log(dataList);
   })
   .finally(() => {
     getConnectionFromPool('SELECT FOUND_ROWS() as total')
